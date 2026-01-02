5549f0d32851d448a508801b9367de883b679d1e
Your Name <user@example.com>
2026-01-02 19:32:22 +0100

    Add cleanmymac homebrew

---
+++ homebrew-custom/Casks/cleanmymac.rb
@@ -0,0 +1,11 @@
+cask "cleanmymac" do
+  version 4
+  sha256 :no_check
+
+  url "https://dl.devmate.com/com.macpaw.CleanMyMac4/CleanMyMacX.dmg"
+  name "CleanMyMac X 4"
+  desc "Tool to remove unnecessary files and free up disk space"
+  homepage "https://macpaw.com/cleanmymac"
+
+  app "CleanMyMac X.app"
+end
